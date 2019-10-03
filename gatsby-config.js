67c0e1c359be95a19b1cb89608a84fb88ea14745
arthur-es <arthurcastroes@gmail.com>
2019-10-03 15:26:47 -0300

    bulma added to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  siteMetadata: {
-    title: "Arthur Castro",
-    author: "@accszar",
-    siteUrl: "https://arthur-es.github.io/"
-  },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+    siteMetadata: {
+        title: "Arthur Castro",
+        author: "@accszar",
+        siteUrl: "https://arthur-es.github.io/",
+    },
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-sass",
+    ],
 };
